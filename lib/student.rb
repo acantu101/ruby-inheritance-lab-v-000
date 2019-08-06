4a12369721751b8f491ad7f6e1496608021b43b6
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 17:16:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def intialize(knowledge)
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
